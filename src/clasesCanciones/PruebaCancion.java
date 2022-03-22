2f05f7871c7ddc52aaddb63c67361e89c1b75e6d
IvanTejedor <ivan.tejedor-garcia@iessampedro.org>
2022-03-22 16:39:11 +0100

    Segundo commit

--- src/clasesCanciones/PruebaCancion.java
+++ src/clasesCanciones/PruebaCancion.java
@@ -34,7 +34,12 @@ public class PruebaCancion {
 		Cancion segundaCancion = new Cancion("Don’t Stop Believin’", "Journey", 320, false);
 		
 		Cancion terceraCancion = new Cancion("Come Join The Murder", "The White Buffalo & The Forest Rangers", 420, false);
+		
+		Cancion cuartaCancion = new Cancion("The four horsemen", "Metallica", 433, false);
+		
+		Cancion quintaCancion = new Cancion("My Flaws Burn Through My Skin Like Demonic Flames From Hell", "$uicideboy$", 167, false);
 
+		
 		if (primeraCancion.reproducirCancion()) {
 			
 			System.out.println(primeraCancion.getTitulo() + "está sonando");
